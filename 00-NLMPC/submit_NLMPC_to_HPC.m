54e2f001d60e704b6f664daee9b27ede04b5fc38
EdwardBras <20068530@sun.ac.za>
2025-03-26 16:16:52 +0200

    Added script used to submit closed-loop NLMPC simulation for the quadruple tank process to the SU HPC.

---
+++ 00-NLMPC/submit_NLMPC_to_HPC.m
@@ -0,0 +1,9 @@
+%% Code used to submit a script to the SU HPC (parallel computations).
+%% Date: 2024-10-16
+
+c = parcluster(); % initialize cluster using the default profile
+c.AdditionalProperties.Host = 'comp029'; 
+ncpu = 16; % number of cpu cores
+
+% submit job to the SU cluster
+test_NLMPC_qudruple_tank = batch(c,'MIMO_NLMPC_control_quadruple_tank','Pool',ncpu,'CaptureDiary',true);
