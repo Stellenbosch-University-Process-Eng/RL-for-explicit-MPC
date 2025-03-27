84a95eea934b31d92e57eae9b3aef199eec5f9bb
EdwardBras <20068530@sun.ac.za>
2025-03-27 14:15:24 +0200

    Streamlined the use of certain variables, renamed variables, changed comments, and made other minor code adjustments.

--- 02-Warm starts/generate_optimal_cost_and_policy_data.m
+++ 02-Warm starts/generate_optimal_cost_and_policy_data.m
@@ -8,8 +8,8 @@ tic
 
 %% load matrix of coordinates (2024-02-08)
 nmberLevels = 5;
-nmberVariables = 6;
-myCoordMat = zeros(nmberLevels^nmberVariables,nmberVariables);
+nmberRLStates = 6; % states 1-6
+myCoordMat = zeros(nmberLevels^nmberRLStates,nmberRLStates);
 totalCntr = 1;
 for cntr_1 = 1:1:nmberLevels
     for cntr_2 = 1:1:nmberLevels
@@ -34,9 +34,9 @@ nu = 2; % measured disturbances, MV, unmeasured disturbances
 nlobj = nlmpc(nx,ny,nu); % create the non-linear MPC object
 
 %% specifying controller parameters
-nlobj.Ts = 1; % set the sample time within the MPC object
-nlobj.PredictionHorizon = 300;%10;  % prediction horizon
-nlobj.ControlHorizon = 2;     %5;      % number of steps to adjust across the horizon
+nlobj.Ts = 1;                           % set the sample time within the MPC object
+nlobj.PredictionHorizon = 300;          % prediction horizon
+nlobj.ControlHorizon = 2;               % number of steps to adjust across the horizon
 
 %% define parameters for model
 param.A1 = 28;      % cross-sectional area (cm^2)
@@ -48,16 +48,18 @@ param.a3 = param.a1;
 param.a2 = 0.057;
 param.a4 = param.a2;
 param.g = 981;       % gravitational acceleration (cm/s^2)
-param.k1 = 3.14;     % pump 1 gain (cm^3/V)
-param.k2 = 3.29;     % pump 2 gain (cm^3/V)
-param.gamma1 = 0.43;%0.4;%0.42; % fraction opening pump 1 three-way valve (-)
-param.gamma2 = 0.34;%0.4;%0.35; % fraction opening pump 2 three-way valve (-)
+param.k1 = 3.33;     % pump 1 gain (cm^3/V)
+param.k2 = 3.35;     % pump 2 gain (cm^3/V)
+param.gamma1 = 0.43; % fraction opening pump 1 three-way valve (-)
+param.gamma2 = 0.34; % fraction opening pump 2 three-way valve (-)
 
 %% specifying dynamic model
+% https://www.mathworks.com/help/mpc/ug/specify-prediction-model-for-nonlinear-mpc.html
 nlobj.Model.StateFcn = @(x,u,p) myStateFunction(x,u,param);  % specify model used to generate next states
 nlobj.Model.NumberOfParameters = 1; % set the number of optional parameters equal to one (the parameter is the structure "p" which contains the model parameters)
 
 %% specify Jacobian for dynamic model
+% https://www.mathworks.com/help/mpc/ug/specify-prediction-model-for-nonlinear-mpc.html#mw_6eb5a593-c403-47f7-b2d8-fed832e17a61
 nlobj.Jacobian.StateFcn = @(x,u,p) myStateJacobian(x,u,param); % specify Jacobian for the predictive model
 
 %% specify initial state, SP, and prediction model inputs
@@ -68,19 +70,19 @@ h_2_SS_initial_guess = 15.70;
 h_3_SS_initial_guess = 6.05;
 h_4_SS_initial_guess = 9.28;
 
-%% simulate the non-linear process model
+%% find model steady state
 final_time = 1e6;
 tspan = linspace(0,final_time,final_time); % time span (s)
-[~,Output] = ode23s(@(t,x) QTProcess_NL_const_time(t,x,param,v_1_SS,v_2_SS),tspan,[h_1_SS_initial_guess,h_2_SS_initial_guess,h_3_SS_initial_guess,h_4_SS_initial_guess]');%,opts);
+[~,Output] = ode23s(@(t,x) QTProcess_NL_const_time(t,x,param,v_1_SS,v_2_SS),tspan,[h_1_SS_initial_guess,h_2_SS_initial_guess,h_3_SS_initial_guess,h_4_SS_initial_guess]');
 
-%% save steady states
+% extract steady states from simulation output
 h_1_SS = Output(end,1);
 h_2_SS = Output(end,2);
 h_3_SS = Output(end,3);
 h_4_SS = Output(end,4);
 
 x0 = [h_1_SS,h_2_SS,h_3_SS,h_4_SS]'; % nominal states
-u0 = [3,3]'; % nominal inputs to the model
+u0 = [v_1_SS,v_2_SS]'; % nominal inputs to the model
 SP = [h_1_SS,h_2_SS,0,0]; % set point
 
 %% linear constraints on the MVs
@@ -105,7 +107,7 @@ param.MVr = [0,0;0,0]; % weighting matrix for rate of control input adjustments 
 %% https://www.mathworks.com/help/mpc/ug/specify-cost-function-for-nonlinear-mpc.html
 data.Ts = nlobj.Ts; % sampling period
 data.CurrentStates = x0; % current states
-data.LastMV = u0(1); % last control input
+data.LastMV = u0; % last control input
 data.References = SP; % set point
 data.MVTarget = []; 
 data.PredictionHorizon = nlobj.PredictionHorizon;
@@ -121,18 +123,15 @@ e = 0;       % only hard constraints are applicable
 % information and tips on constraint softening and the tuning of Equal
 % Concern for Relaxation (ECR) values.
 
-% params = []; % no parameters in cost function
-param.S_tracking = 1;%nlobj.States(1).Max - nlobj.States(1).Min; % scale factor for SP tracking cost (2023-11-13)
+% also see https://www.mathworks.com/help/mpc/ug/optimization-problem.html
+% for discussion of default MPC cost functions
+
+param.S_tracking = 1; % scale factor for SP tracking cost (2023-11-13)
 param.S_MV = nlobj.MV(1).Max - nlobj.MV(1).Min; % scale factor for MV adjustment (2023-11-13)
 param.S_MV_rate = 1;%nlobj.MV(1).Max - nlobj.MV(1).Min; % scale factor used for rate of MV adjustment (2023-11-13)
 param.MV_ref = 0; % reference for MV tracking penalty (2023-11-13)
 
-%% load state scaling structure for cost calculation
-% filename_state_scaling_structure = 'C:\Users\Edward\Stellenbosch University\Machine Learning at Process Engineering - Edward Bras (1)\Code\PhD code\12-Article 2\00-Data\03-Runs with warm-started critic\00-Minimum phase region\P_+_state_scaling_data.mat'; % state scaling
-% load(filename_state_scaling_structure); % load state scaling data
-% param.PS_input = PS_input;
-
-%%
+%% https://www.mathworks.com/help/optim/ug/tolerances-and-stopping-criteria.html
 nlobj.Optimization.CustomCostFcn = @(X,U,e,data,params) CostFunction_for_two_states(X,U,e,data,params); % (2023-10-28)
 nlobj.Optimization.ReplaceStandardCost = true;
 
@@ -145,15 +144,13 @@ nlobj.Optimization.SolverOptions.OptimalityTolerance = 1e-1;
 nlobj.Optimization.SolverOptions.FunctionTolerance = 1e-1;
 nlobj.Optimization.SolverOptions.MaxIterations = 40;
 
-% nlobj.Optimization.SolverOptions.Algorithm = 'active-set';
-
 %% validate the prediction model's functions
 validateFcns(nlobj,x0,u0,[],{param}); % validate -> nlobj = object, x0 = starting states, u0 = control inputs, [] = no measured disturbances, Ts = an optional parameter
 
 %% simulate one optimization across the prediction horizon
 nloptions = nlmpcmoveopt;
 nloptions.Parameters = {param};
-[~,~,Info_OL] = nlmpcmove(nlobj,x0,u0,SP,[],nloptions); % nlmpcmove(nlobj,x0,u0(1),SP,u0(2),nloptions);
+[~,~,Info_OL] = nlmpcmove(nlobj,x0,u0,SP,[],nloptions); 
 
 J_OL_func = @(x_1,x_2) (x_1 - SP(1)).^2 + (x_2 - SP(2)).^2;
 
@@ -163,18 +160,8 @@ for cntr = 1:1:( size(Info_OL.Xopt,1) - 1 )
     X_2_OL_traj(cntr) = Info_OL.Xopt(cntr+1,2);
 end
 
-%% simulate closed-loop control under non-linear MPC
-simulationTime = 200;  % number of time steps to simulate
-x_CL_trajectory = x0';       % initialize liquid height trajectory 
-u_CL_trajectory = u0';   % initialize MV trajectory
-SP_trajectory = [SP(1),SP(2),0,0];        % initialize SP trajectory
-
-%% number of entries in tspan vector
-nmberTspanEntries = 100;
-
 %% Create grid to evaluate optimal solution over
-resolution4Grid_except_SP_2 = nmberLevels;%5;    % (2023-11-23)
-resolution4Grid_SP_2 = nmberLevels;%5;           % number of SPs for regulation portion of problem (2023-11-23)
+resolution4Grid = nmberLevels;             % total number of optimisations will be resolution4Grid^nmberRLStates
 
 % maximum and minimum states expected (states 1 through 4)
 minimum_SP_1 = 0;
@@ -194,35 +181,34 @@ minimum_H_4 = 0;
 maximum_H_3 = 50;
 maximum_H_4 = 50;
 
-SP_1_dimension = linspace(minimum_SP_1,maximum_SP_1,resolution4Grid_except_SP_2); % dimension for H1 SP
-SP_2_dimension = linspace(minimum_SP_2,maximum_SP_2,resolution4Grid_SP_2); % dimension for H2 SP
-H_1_dimension = linspace(minimum_H_1,maximum_H_1,resolution4Grid_except_SP_2);  % dimension for H1 
-H_2_dimension = linspace(minimum_H_2,maximum_H_2,resolution4Grid_except_SP_2);  % dimension for H2
-H_3_dimension = linspace(minimum_H_3,maximum_H_3,resolution4Grid_except_SP_2); % dimension for H3
-H_4_dimension = linspace(minimum_H_4,maximum_H_4,resolution4Grid_except_SP_2);  % dimension for H4
+SP_1_dimension = linspace(minimum_SP_1,maximum_SP_1,resolution4Grid);   % dimension for H1 SP
+SP_2_dimension = linspace(minimum_SP_2,maximum_SP_2,resolution4Grid);   % dimension for H2 SP
+H_1_dimension = linspace(minimum_H_1,maximum_H_1,resolution4Grid);      % dimension for H1 
+H_2_dimension = linspace(minimum_H_2,maximum_H_2,resolution4Grid);      % dimension for H2
+H_3_dimension = linspace(minimum_H_3,maximum_H_3,resolution4Grid);      % dimension for H3
+H_4_dimension = linspace(minimum_H_4,maximum_H_4,resolution4Grid);      % dimension for H4
 
 % set scale factors
-nlobj.States(1).ScaleFactor = 1;%spSample.setPoint_high - spSample.setPoint_low; 
-nlobj.States(2).ScaleFactor = 1;%spSample.setPoint_high - spSample.setPoint_low;
-nlobj.States(3).ScaleFactor = 1;%spSample.setPoint_high - spSample.setPoint_low;
-nlobj.States(4).ScaleFactor = 1;%spSample.setPoint_high - spSample.setPoint_low;
+nlobj.States(1).ScaleFactor = 1; 
+nlobj.States(2).ScaleFactor = 1;
+nlobj.States(3).ScaleFactor = 1;
+nlobj.States(4).ScaleFactor = 1;
 
 [SP_1_grid,SP_2_grid,H_1_grid,H_2_grid,H_3_grid,H_4_grid] = ndgrid(SP_1_dimension,SP_2_dimension,H_1_dimension,H_2_dimension,H_3_dimension,H_4_dimension);
 
-x_k = zeros(4,1); % state comprises four liquid heights and two SPs (for liquid heights one and two) (2023-10-31)
+x_k = zeros(4,1); % RL states comprise four liquid heights and two SPs (for liquid heights one and two) 
 
-%% Determine optimal solution for constant inlet flow rate
-nmberOpts = resolution4Grid_SP_2*resolution4Grid_except_SP_2^5; % number of optimizations to be solved (2023-11-16)
+%% perform optimisations using predefined matrix of indices (2024-02-08)
+nmberOpts = resolution4Grid^nmberRLStates; % number of optimizations to be solved (2023-11-16)
 cntr_wait = 0; % initialize counter for waitbar (2023-11-16)
 % H = waitbar(0,'Please wait...');
 
-%% vectorize nested for-loop using predefined matrix of coordinates (2024-02-08)
-size_vec = resolution4Grid_except_SP_2.*ones(1,6);
+size_vec = resolution4Grid.*ones(1,nmberRLStates);
 u_opt_one = zeros(size_vec);
 u_opt_two = u_opt_one;
 optimalCosts = u_opt_one; % initialize vector that will be used to store optimal costs (2024-04-25)
 
-for cntr = 1:1:resolution4Grid_except_SP_2^6
+for cntr = 1:1:resolution4Grid^nmberRLStates
     row = myCoordMat(cntr,:); % obtain coordinate containing the levels of the different states
     
     %% select SP and DV
@@ -331,17 +317,17 @@ end
 % Specify analytical Jacobian for predictive model
 function [A,Bmv] = myStateJacobian(x,u,param)
     A(1,1) = -1*( param.a1/(2*param.A1) )*sqrt(2*param.g)*x(1)^-0.5;
-    A(1,3) = ( param.a1/(2*param.A1) )*sqrt(2*param.g)*x(3)^-0.5;
+    A(1,3) = ( param.a3/(2*param.A1) )*sqrt(2*param.g)*x(3)^-0.5;
     Bmv(1,1) = param.gamma1*param.k1/param.A1;
 
     A(2,2) = -1*( param.a2/(2*param.A2) )*sqrt(2*param.g)*x(2)^-0.5;
-    A(2,4) = ( param.a2/(2*param.A2) )*sqrt(2*param.g)*x(4)^-0.5;
+    A(2,4) = ( param.a4/(2*param.A2) )*sqrt(2*param.g)*x(4)^-0.5;
     Bmv(2,2) = param.gamma2*param.k2/param.A2;
 
-    A(3,3) = -1*(param.a1/(2*param.A1))*sqrt(2*param.g)*x(3)^-0.5;
-    Bmv(3,2) = (1-param.gamma2)*param.k2/param.A1;
+    A(3,3) = -1*(param.a3/(2*param.A1))*sqrt(2*param.g)*x(3)^-0.5;
+    Bmv(3,2) = (1-param.gamma2)*param.k2/param.A3;
 
-    A(4,4) = -1*(param.a2/(2*param.A2))*sqrt(2*param.g)*x(4)^-0.5;
-    Bmv(4,1) = (1-param.gamma1)*param.k1/param.A2;
+    A(4,4) = -1*(param.a4/(2*param.A2))*sqrt(2*param.g)*x(4)^-0.5;
+    Bmv(4,1) = (1-param.gamma1)*param.k1/param.A4;
 
 end
