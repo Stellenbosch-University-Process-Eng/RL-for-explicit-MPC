fdb6db8c20903afb7cd78aeb15c2af59cad9c698
EdwardBras <20068530@sun.ac.za>
2025-04-08 14:48:16 +0200

    Changed parameter values of process model.

--- 01-QT model/simulate_QTProcess.m
+++ 01-QT model/simulate_QTProcess.m
@@ -6,17 +6,17 @@
 clc;clear;close all;
 
 %% define parameters for model
-param.A1 = 28;  % cross-sectional area (cm^2)
+param.A1 = 10;  % cross-sectional area (cm^2)
 param.A3 = param.A1; 
-param.A2 = 32;
+param.A2 = 10;
 param.A4 = param.A2;
 param.a1 = 0.071; % cross-section of tank outlet (cm^2)
 param.a3 = param.a1;
-param.a2 = 0.057;
+param.a2 = 0.071;
 param.a4 = param.a2;
 param.g = 981;       % gravitational acceleration (cm/s^2)
 param.k1 = 3.33;     % pump 1 gain (cm^3/V)
-param.k2 = 3.35;     % pump 2 gain (cm^3/V)
+param.k2 = 3.33;     % pump 2 gain (cm^3/V)
 param.gamma1 = @(t) 0.3 + 0*t; % fraction opening pump 1 three-way valve (-)
 param.gamma2 = @(t) 0.3 + 0*t; % fraction opening pump 2 three-way valve (-)
 
