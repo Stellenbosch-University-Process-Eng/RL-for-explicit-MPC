0da7f1ad3f6b8325914468c80aea73890b62c6f6
EdwardBras <20068530@sun.ac.za>
2025-06-10 22:43:42 +0200

    Evolving process dynamics in non-minimum phase operating region (10 scenarios, 10 actor learning rates, no reward scaling, nominal αR, warm-started critic).

--- 03-RL policy adaptation/many_scenarios_explicit_MPC_RL_adaptation.m
+++ 03-RL policy adaptation/many_scenarios_explicit_MPC_RL_adaptation.m
@@ -53,12 +53,12 @@ for scenarioCntr = 1:1:numberScenarios
     
     %% load critic network
     critic.NN = value_data.NN;
-    critic.nmber_input_nodes = size(critic.NN.hidden_layer_parameters,1) - 1;
-    critic.nmber_hidden_nodes = size(critic.NN.hidden_layer_parameters,2);
-    critic.NN.hidden_layer_parameters = randn(critic.nmber_input_nodes + 1, critic.nmber_hidden_nodes)*sqrt(2/critic.nmber_input_nodes);
-    critic.NN.output_layer_parameters = randn(critic.nmber_hidden_nodes + 1,1)*sqrt(1/critic.nmber_hidden_nodes);
-    critic.NN.hidden_layer_parameters(1,:) = 0;
-    critic.NN.output_layer_parameters(1,:) = 0;
+%     critic.nmber_input_nodes = size(critic.NN.hidden_layer_parameters,1) - 1;
+%     critic.nmber_hidden_nodes = size(critic.NN.hidden_layer_parameters,2);
+%     critic.NN.hidden_layer_parameters = randn(critic.nmber_input_nodes + 1, critic.nmber_hidden_nodes)*sqrt(2/critic.nmber_input_nodes);
+%     critic.NN.output_layer_parameters = randn(critic.nmber_hidden_nodes + 1,1)*sqrt(1/critic.nmber_hidden_nodes);
+%     critic.NN.hidden_layer_parameters(1,:) = 0;
+%     critic.NN.output_layer_parameters(1,:) = 0;
 
     critic.NN.alpha = 0.5; % critic learning rate
     
@@ -66,7 +66,7 @@ for scenarioCntr = 1:1:numberScenarios
     %% structures as fields of the parameter structure
     param.PS_input = policy_data.PS_input; 
     param.PS_targets = policy_data.PS_targets;
-%     param.PS_Value_targets = value_data.PS_Value_targets;
+    param.PS_Value_targets = value_data.PS_Value_targets;
     
     %% initialize average reward and relevant learning rate (2023-06-29)
     param.avgRAlpha = 0.5;     % learning rate used to update the average reward (2023-06-29)
@@ -218,13 +218,13 @@ end % end loop through scenarios
 % delete(myPool)
 
 %% save results
-non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1.Experience = all_scenarios_out_Experience; % save experience generated during training
-non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1.Policies = all_scenarios_out_Policies;     % save policy networks generated during training
-non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1.logging.model_parameters = param;          % save dynamic model's parameters
-non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1.logging.seed = sim_seed;
+non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1.Experience = all_scenarios_out_Experience; % save experience generated during training
+non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1.Policies = all_scenarios_out_Policies;     % save policy networks generated during training
+non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1.logging.model_parameters = param;          % save dynamic model's parameters
+non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1.logging.seed = sim_seed;
 
-filename = '/scratch3/20068530/non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1';
-save(filename,'non_min_phase_decr_gamma_scratch_V_ten_scen_batch_1',"-v7.3");
+filename = '/scratch3/20068530/non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1';
+save(filename,'non_min_phase_decr_gamma_nom_alphaR_ten_scen_batch_1',"-v7.3");
 
 toc % moved 2022-10-17
 
@@ -504,8 +504,8 @@ function [temporal_diff,yrnj,p,z_rnj_hidden,z_rnj_output] = calculateTemporalDif
     [V_S_crnt,z_rnj_hidden,yrnj,z_rnj_output] = evaluate_ReLU_tanh_six_states_one_output(NN,State_1,State_2,State_3,State_4,State_5,State_6); % evaluate critic network at the current state
     [V_S_nxt,~,~,~] = evaluate_ReLU_tanh_six_states_one_output(NN,nxtState_1,nxtState_2,nxtState_3,nxtState_4,nxtState_5,nxtState_6); % evaluate critic network at the next state
 
-%     [V_S_crnt,~] = mapminmax('reverse',V_S_crnt,p.PS_Value_targets);
-%     [V_S_nxt,~] = mapminmax('reverse',V_S_nxt,p.PS_Value_targets);
+    [V_S_crnt,~] = mapminmax('reverse',V_S_crnt,p.PS_Value_targets);
+    [V_S_nxt,~] = mapminmax('reverse',V_S_nxt,p.PS_Value_targets);
 
     temporal_diff = R - p.avgR + V_S_nxt - V_S_crnt; % calculate the temporal difference in the average reward setting (2023-06-29)
     p = updateAverageReward(p,temporal_diff); % update the 
