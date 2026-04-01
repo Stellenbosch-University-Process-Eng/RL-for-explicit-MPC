193a57064b5f9a5dc6032f31d4bdea40210c75a1
EdwardBras <20068530@sun.ac.za>
2026-04-01 12:32:41 +0200

    Added "close all" statement.

--- 01-QT model/compute_zero_data.m
+++ 01-QT model/compute_zero_data.m
@@ -4,7 +4,7 @@
 %% Date: 2025-10-04
 
 %%
-clc;clear;%close all;
+clc; clear; close all;
 
 %% define valve positions
 num_gammas = 200;    % number of valve position for which to determine zero locations
